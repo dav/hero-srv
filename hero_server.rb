91640f4861542dd881740ff488f450f714be3e17
Dav Yaginuma <dav@akuaku.org>
2014-03-02 17:06:04 -0800

    Added correct pg url

--- hero_server.rb
+++ hero_server.rb
@@ -18,7 +18,7 @@ configure :development do
 end
 
 configure :production do
-    DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_RED_URL'])
+    DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_GRAY_URL'])
 end
 
 #################
